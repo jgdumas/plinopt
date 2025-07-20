4a0ff6e71bf86b1601a78005db01917c8d69b168
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2025-07-20 21:22:26 +0200

    nomore parenthesis in sign swaps

--- bin/sign-swap.sh
+++ bin/sign-swap.sh
@@ -10,4 +10,4 @@
 var=$1
 fil=$2
 echo $0 ${var} ${fil}
-sed "s/${var}/S${var}S/g;s/-S${var}S/M${var}/g;s/+S${var}S/P${var}/g;s/S${var}S/P${var}/g;s/M${var}/+${var}/g;s/P${var}/-${var}/g;s/:=+${var}/:=${var}/g;s/-${var}:=.*;/&)/;s/;)/);/;s/-${var}:=/${var}:=-(/;s/:=(.*)/PAPAPAPA&PAPAPAPA/;s/PAPAPAPA:=(/:=/;s/)PAPAPAPA//" ${fil} | compacter -s | sed 's/:=(.*)/PAPAPAPA&PAPAPAPA/;s/PAPAPAPA:=(/:=/;s/)PAPAPAPA//' | compacter -s | sponge ${fil}
+sed "s/${var}/S${var}S/g;s/-S${var}S/M${var}/g;s/+S${var}S/P${var}/g;s/S${var}S/P${var}/g;s/M${var}/+${var}/g;s/P${var}/-${var}/g;s/:=+${var}/:=${var}/g;" ${fil} | sed "/-${var}:=/{ s/:=[^+-]/+&/;s/+:=/:=+/;s/+/PLUS/g;s/-/+/g;s/PLUS/-/g;s/+${var}:=/${var}:=/ }" | compacter -s | sponge ${fil}
