d6ce611bd04077cf809d1db4d406f466e92f1d59
Jean-Guillaume Dumas <Jean-Guillaume.Dumas@imag.fr>
2026-05-27 11:54:09 +0200

    inplace singlevars

--- bin/singlevar.sh
+++ bin/singlevar.sh
@@ -9,16 +9,27 @@
 
 FIL=
 CHR=
+STR=10
+INP=0
 
 while [[ $# -gt 0 ]]; do
   case $1 in
-    -c)
+    -c|-z|--char)
     CHR="$2"
     shift # past argument
     shift # past value
     ;;
+    -s|--start)
+    STR="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -i|--inplace)
+    INP=1
+    shift # past argument
+    ;;
     -h)
-      echo "Usage: $0 [-c x] f.slp"
+      echo "Usage: $0 [-c|-z x] [-s #] [-i] f.slp"
       exit 1
       ;;
     *)
@@ -30,35 +41,46 @@ done
 
 SLP=
 if [[ "$FIL" == "" ]]; then
+  INP=0
   SLP=$(cat)
 else
   SLP=`cat ${FIL}`
 fi
 
 RDS=`echo "${SLP}" | egrep -v '(^o|^A|^B|^C)'`
+VARS=(`echo "${RDS}" | sed -r 's/(.).*/\1/' | sort -u | tr '\n' ' '`)
+# echo "VARS: ${VARS[@]}"
 
 if [[ "$CHR" == "" ]]; then
-  VARS=(`echo "${RDS}" | sed -r 's/(.).*/\1/' | sort -u | tr '\n' ' '`)
-#   echo "VARS: ${VARS[@]}"
 
   CHR='d'
   while grep -q ${CHR} <<< ${VARS[@]}; do
     CHR=`echo ${CHR} | tr "a-z" "b-za"`
   done
 
-#   echo "CHR: ${CHR}"
 fi
 
+# echo "CHR: ${CHR}"
+>&2 echo -n "# [SingleVars] variables ${VARS[@]}"
 VARS+=(${CHR})
 ZHR='d'
 while grep -q ${ZHR} <<< ${VARS[@]}; do
     ZHR=`echo ${ZHR} | tr "a-z" "b-za"`
 done
 
-#   echo "ZHR: ${ZHR}"
 
-SED=`echo "${RDS}" | sed -r "s/${CHR}/${ZHR}/g;s/:=.*//" | sort -u | tac | awk -v chr=${CHR} 'BEGIN{s=10;ORS=""} {print "s/"$1"/"chr""s"/g;";s++}'`
+SED=`echo "${RDS}" | sed -r "s/${CHR}/${ZHR}/g;s/:=.*//" | sort -u | tac | awk -v chr=${CHR} -v str=${STR} 'BEGIN{s=str;ORS=""} {print "s/"$1"/"chr""s"/g;";s++}'`
 
+LSV=$(echo ${SED} | sed "s/.*${CHR}//;s/\/.*//")
+NBV=$((LSV-STR+1))
+>&2 echo " (${NBV})  -->  ${CHR}${STR} .. ${CHR}${LSV}"
+
+# echo "ZHR: ${ZHR}"
 # echo "SED: ${SED}"
+# echo "CHG: s/${CHR}/${ZHR}/g;${SED}"
 
-echo "${SLP}" | sed "s/${CHR}/${ZHR}/g;${SED}"
+if [[ ${INP} -eq 1 ]]; then
+  sed -i "s/${CHR}/${ZHR}/g;${SED}" ${FIL}
+else
+  echo "${SLP}" | sed "s/${CHR}/${ZHR}/g;${SED}"
+fi
